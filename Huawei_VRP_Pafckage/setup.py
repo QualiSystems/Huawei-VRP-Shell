daa8b317cbf0470e44c3ac4090ebc00b9646a408
QS-IL-LUIZAN$ <luiza.n@quali.com>
2016-07-19 11:19:03 +0300

    package

---
+++ Huawei_VRP_Pafckage/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+with open(os.path.join('version.txt')) as version_file:
+    version_from_file = version_file.read().strip()
+
+with open('requirements.txt') as f_required:
+    required = f_required.read().splitlines()
+
+
+setup(
+    name='Huawei-VRP-Shell',
+    url='https://github.com/QualiSystems/Huawei-VRP-Shell',
+    author='QualiSystems',
+    author_email='info@qualisystems.com',
+    packages=find_packages(),
+    install_requires=required,
+    test_suite='tests',
+    tests_require=required_for_tests,
+    version=version_from_file,
+    description='QualiSystems Python package',
+    include_package_data = True
+)
